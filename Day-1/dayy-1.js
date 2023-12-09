70240cb986031ab2baaaa79f453e41ec1c44f0c1
SRI VARSHINI <srivarshinir23@gmail.com>
2023-12-09 12:29:25 +0530

    Day-1 Loginpage

---
+++ Day-1/dayy-1.js
@@ -0,0 +1,8 @@
+function register(){
+    let x = document.getElementById("userName").value;
+    let a = document.getElementById("email").value;
+    let b = document.getElementById("pwd1").value;
+    let c = document.getElementById("number").value;
+    alert("you have registered Successfully");
+
+}
