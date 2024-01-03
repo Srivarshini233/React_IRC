2f863836fb6ddad610770e2c916b792b8e934fc8
SRI VARSHINI <srivarshinir23@gmail.com>
2024-01-03 09:38:52 +0530

    music

---
+++ MUSIC-APP/musicplayer/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './assets/Layout.css';
+import './App.css'
+import Layout from './components/Layout'
+
+function App() {
+ 
+  return (
+    <>
+      <Layout></Layout>
+    </>
+  )
+}
+
+export default App
